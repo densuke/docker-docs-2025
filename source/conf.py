20226d0adca3b2b967de6e04d03bfd6826eeb909
SATO Daisuke <densuke@fuga.jp>
2025-02-21 02:16:45 +0000

    sphinxスターターコード

---
+++ source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'アプリケーション・サーバー構築1 資料'
+copyright = '2025, 佐藤 大輔 <densuke@st.kobedenshi.ac.jp>'
+author = '佐藤 大輔 <densuke@st.kobedenshi.ac.jp>'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ja'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
